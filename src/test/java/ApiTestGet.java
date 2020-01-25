35d2efcbb7036d9f1f19d7a56489b8a46439c690
DashL <blwkkz@gmail.com>
2020-01-25 17:13:39 +0300

    RequestSpecBuilder

--- src/test/java/ApiTestGet.java
+++ src/test/java/ApiTestGet.java
@@ -1,10 +1,25 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.*;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class ApiTestGet {
+
+    private static RequestSpecification requestSpec;
+
+    @BeforeClass
+    public static void createRequestSpecification() {
+
+        requestSpec = new RequestSpecBuilder().
+                setBaseUri("http://httpbin.org").
+                build();
+    }
+
     @Test()
     public void testGet() {
-        get("http://httpbin.org/get?a=1").then().body("args.a", equalTo("1"));
+        given().spec(requestSpec)
+                .when().get("/get?a=1")
+                .then().body("args.a", equalTo("1")).statusCode(200);
     }
 }
